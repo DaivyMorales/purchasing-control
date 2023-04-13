a57b7027bb1d3e3cb827d2a5b55b0c2f7bfa0b4b
Daivy Morales <daivymorales7705@gmail.com>
2023-04-13 10:34:58 -0500

    Connect with DB

---
+++ src/utils/database.ts
@@ -0,0 +1,29 @@
+import { connect, connection } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+type Conn = {
+  isConnected: boolean;
+};
+
+const conn: Conn = {
+  isConnected: false,
+};
+
+export const dbConnect = async () => {
+  if (conn.isConnected) return;
+
+  const db = await connect(process.env.DB_LOCAL || "");
+
+  conn.isConnected = !!db.connections[0].readyState;
+
+  console.log(db.connection.db.databaseName);
+};
+
+connection.on("connected", () => {
+  console.log("DB CONNECTED!");
+});
+
+connection.on("ERROR", (err) => {
+  console.log(err);
+});
