cc9a8882b80509be1507854715de6daccff30a52
Daivy Morales <daivymorales7705@gmail.com>
2023-04-19 15:01:58 -0500

    Change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  async rewrites() {
+  async headers() {
     return [
       {
+        // Allow requests from any origin
         source: "/api/:path*",
-        destination: "https://purchasing-control.vercel.app/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With,Accept,Content-Type",
+          },
+        ],
       },
     ];
   },
-};
-
-module.exports = {
-  nextConfig,
 };
