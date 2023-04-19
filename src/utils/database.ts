66e08adcb5b5f261a62e6d089b87034de02a24cc
Daivy Morales <daivymorales7705@gmail.com>
2023-04-19 15:33:51 -0500

    Change to Cloud DB

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -13,7 +13,7 @@ const conn: Conn = {
 export const dbConnect = async () => {
   if (conn.isConnected) return;
 
-  const db = await connect(process.env.DB_LOCAL || "");
+  const db = await connect(process.env.DB_URL_DEPLOYED || "");
 
   conn.isConnected = !!db.connections[0].readyState;
 
