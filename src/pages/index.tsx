5e07d80c2d3be1b24804fe74372184ff2673f202
Daivy Morales <daivymorales7705@gmail.com>
2023-04-19 18:28:07 -0500

    Add testing vercel URL in upload Excel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,7 @@ export default function index({ data }: MyProps) {
       const dataExcel: Array<IData> = xlsx.utils.sheet_to_json(worksheet);
       try {
         const response = await axios.post(
-          "http://localhost:3000/api/inventory",
+          "https://purchasing-control-git-testing-deploy-daivymorales-s-team.vercel.app/api/inventory",
           dataExcel
         );
         setInformation(response.data);
